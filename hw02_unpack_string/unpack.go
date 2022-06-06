27141ca1d5c5cdbd5db90b4682be75ede2e6c409
Sergey Fedorov <s.fedorov@propellerads.net>
2022-06-06 10:56:13 +0300

    hw02_unpack_string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,58 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	if str == "" {
+		return "", nil
+	}
+	if !validate(str) {
+		return "", ErrInvalidString
+	}
+
+	var strBuilder strings.Builder
+	for _, v := range str {
+		if !unicode.IsDigit(v) {
+			strBuilder.WriteString(string(v))
+			continue
+		}
+
+		sbString := strBuilder.String()
+		curLet := string(sbString[len(sbString)-1])
+		curFactor, _ := strconv.Atoi(string(v - 1))
+		if curFactor == 0 {
+			str = sbString[:len(sbString)-1]
+			strBuilder.Reset()
+			strBuilder.WriteString(str)
+			continue
+		}
+		curStr := strings.Repeat(curLet, curFactor)
+		strBuilder.WriteString(curStr)
+	}
+
+	return strBuilder.String(), nil
+}
+
+func validate(str string) bool {
+	if unicode.IsDigit(rune(str[0])) {
+		return false
+	}
+	for i, v := range str {
+		if !unicode.IsDigit(v) {
+			continue
+		}
+		if next := string(str[i+1]); next != "" {
+			if unicode.IsDigit(rune(next[0])) {
+				return false
+			}
+		}
+	}
+
+	return true
 }
