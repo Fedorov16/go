49c3ec34d39328569dab0b2122d0600e93b9954f
Sergey Fedorov <57595919+Fedorov16@users.noreply.github.com>
2022-06-21 17:02:09 +0300

    hw02_unpack_string (#2)

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,61 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	if regexpValidate(str) != nil {
+		return "", regexpValidate(str)
+	}
+
+	splitByRegexp := SplitByRegex(str)
+
+	var strBuilder strings.Builder
+	var curStr string
+	for _, v := range splitByRegexp {
+		if string(v[0]) == "\\" {
+			if len(v) == 3 {
+				curStr = v[len(v)-2 : len(v)-1]
+				curFactor, _ := strconv.Atoi(string(v[len(v)-1]))
+				if curFactor != 0 {
+					preparedStr := strings.Repeat(curStr, curFactor)
+					strBuilder.WriteString(preparedStr)
+				}
+				continue
+			}
+			strBuilder.WriteString(string(v[len(v)-1]))
+			continue
+		} else if len(v) == 1 {
+			strBuilder.WriteString(v)
+			continue
+		}
+
+		curStr = string(v[0])
+		curFactor, _ := strconv.Atoi(string(v[len(v)-1]))
+		if curFactor != 0 {
+			preparedStr := strings.Repeat(curStr, curFactor)
+			strBuilder.WriteString(preparedStr)
+		}
+	}
+
+	return strBuilder.String(), nil
+}
+
+func regexpValidate(str string) error {
+	if str == "" {
+		return nil
+	}
+	if regexp.MustCompile(`(^\d|[^\\]{1}\d\d|\\[[:alpha:]])`).MatchString(str) {
+		return ErrInvalidString
+	}
+	return nil
+}
+
+func SplitByRegex(str string) []string {
+	return regexp.MustCompile(`[[:alpha:]|[:punct:]]{1}\d{0,2}`).FindAllString(str, -1)
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -14,13 +14,12 @@ func TestUnpack(t *testing.T) {
 	}{
 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
 		{input: "abccd", expected: "abccd"},
+		{input: "fr-3fr", expected: "fr---fr"},
+		{input: "qwe3", expected: "qweee"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
 	}
 
 	for _, tc := range tests {
@@ -33,8 +32,30 @@ func TestUnpack(t *testing.T) {
 	}
 }
 
+func TestCheckRegexpSplitString(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected []string
+	}{
+		{input: "a4bc2d5e", expected: []string{"a4", "b", "c2", "d5", "e"}},
+		{input: "abccd", expected: []string{"a", "b", "c", "c", "d"}},
+		{input: "fr-3fr", expected: []string{"f", "r", "-3", "f", "r"}},
+		{input: "qwe3", expected: []string{"q", "w", "e3"}},
+		{input: "aaa0b", expected: []string{"a", "a", "a0", "b"}},
+		{input: `qwe\4\5`, expected: []string{"q", "w", "e", "\\4", "\\5"}},
+		{input: `qwe\45`, expected: []string{"q", "w", "e", "\\45"}},
+	}
+	for _, stCase := range tests {
+		stCase := stCase
+		t.Run(stCase.input, func(t *testing.T) {
+			result := SplitByRegex(stCase.input)
+			require.Equal(t, stCase.expected, result)
+		})
+	}
+}
+
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "0", "--23--", "_0-9=43", "\\e", "q3\\we"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
