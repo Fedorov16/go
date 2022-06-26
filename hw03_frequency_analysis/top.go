61293216826cc871e27b4bb807b6c2f9faa3e7a6
Sergey Fedorov <s.fedorov@propellerads.net>
2022-06-26 23:45:34 +0300

    hw03_frequency_analysis

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,60 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"strings"
+)
+
+type SD []int
+type SW []string
+
+func (s *SD) Add(sStr *SW, newVal string, newInt int) {
+	if len(*s) == 0 {
+		*s = append(*s, newInt)
+		*sStr = append(*sStr, newVal)
+		return
+	}
+	for i, val := range *s {
+		oldS := *s
+		oldStr := *sStr
+		if newInt > val {
+			// ints
+			partBefore := make([]int, len(oldS[:i]))
+			partAfter := make([]int, len(oldS[i:]))
+			copy(partBefore, oldS[:i])
+			copy(partAfter, oldS[i:])
+			r := append(partBefore, newInt)
+			*s = append(r, partAfter...)
+
+			// strings
+			partStringBefore := make([]string, len(oldStr[:i]))
+			partStringAfter := make([]string, len(oldStr[i:]))
+			copy(partStringBefore, oldStr[:i])
+			copy(partStringAfter, oldStr[i:])
+			rStr := append(partStringBefore, newVal)
+			*sStr = append(rStr, partStringAfter...)
+
+			return
+		}
+	}
+	*s = append(*s, newInt)
+	*sStr = append(*sStr, newVal)
+}
+
+func Top10(text string) []string {
+	matches := strings.Fields(text)
+	m := make(map[string]int)
+	for _, v := range matches {
+		m[v]++
+	}
+	sInt := new(SD)
+	sStr := new(SW)
+	for str, count := range m {
+		sInt.Add(sStr, str, count)
+	}
+	res := *sStr
+	if len(res) == 0 {
+		return nil
+	}
+
+	return res[:10]
 }
