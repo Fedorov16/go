21e77b6b19c5d50ffe4aa8083abf4e36b5ea9103
Sergey Fedorov <57595919+Fedorov16@users.noreply.github.com>
2022-06-30 15:41:46 +0300

    hw03_frequency_analysis (#3)
    * hw03_frequency_analysis
    
    * done
    
    * new tests
    
    * hw03
    
    * hw03
    
    * hw03
    
    * hw03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,55 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type CustomMap struct {
+	word  string
+	count int
+}
+
+const maxRes = 10
+
+var reg = regexp.MustCompile(`[.,!']`)
+
+func Top10(text string) []string {
+	matches := strings.Fields(text)
+	m := make(map[string]int)
+	for _, v := range matches {
+		str := reg.ReplaceAllString(v, "")
+		if str == "-" {
+			continue
+		}
+		m[strings.ToLower(str)]++
+	}
+	sliceCM := make([]CustomMap, 0, len(m))
+	for i, v := range m {
+		sliceCM = append(sliceCM, CustomMap{i, v})
+	}
+
+	sort.Slice(sliceCM, func(i, j int) bool {
+		if sliceCM[i].count == sliceCM[j].count {
+			return sliceCM[i].word < sliceCM[j].word
+		}
+		return sliceCM[i].count > sliceCM[j].count
+	})
+
+	if len(sliceCM) == 0 {
+		return nil
+	}
+
+	maxVal := maxRes
+	if maxRes > len(m) {
+		maxVal = len(m)
+	}
+
+	res := make([]string, maxVal, len(m))
+	for i, v := range sliceCM[:maxVal] {
+		res[i] = v.word
+	}
+
+	return res
 }
