bebef3e52de5401f62cf75215f850399906bd990
Sergey Fedorov <s.fedorov@propellerads.net>
2022-11-08 20:24:15 +0500

    hw07 progressbar import

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -5,7 +5,7 @@ import (
 	"io"
 	"os"
 
-	"github.com/schollz/progressbar/v3"
+	progressbar "github.com/schollz/progressbar/v3"
 )
 
 var ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
